ab62575742a777ad9ba2654f7c4796e4a88867e2
Cibocaz <XiangYuanHuLian@outlook.com>
2024-10-22 22:31:14 +0800

    Add files via upload

---
+++ Code/MinecraftServerControlTerminal/src/main/java/org/xyit/skilfully/Gui/MainGui.java
@@ -0,0 +1,14 @@
+package org.xyit.skilfully.Gui;
+
+import javax.swing.*;
+
+public class MainGui extends JFrame {
+
+    /**
+     * 主界面的Gui方法
+     */
+    public static void MainInterface() {
+        
+    }
+
+}
